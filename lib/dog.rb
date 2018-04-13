e4d60b0839cf5496098827dfbca5fb91760ac943
David Moore <hey@iamdavidmoore.com>
2018-04-13 16:19:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,21 @@
 class Dog
-  #initalizes the Dog instance that can be named
-  def initialize(name)
-    @name = name
-  end
 
-  #Dog GETTER AND SETTER METHODS
-  #name getter method
-  def name
-    @name
-  end
+	#enables Dog to be named
+	def name=(dogs_name)
+		@name = dogs_name
+	end
 
-  #name setter method
-  def name=(new_name)
-    @name = new_name
-  end
+	#enables Dog to be given a breed
+	def breed=(dogs_breed)
+		@breed = dogs_breed
+	end
 
-  #breed getter method
-  def breed
-    @breed
-  end
+	#Dog GETTER/SETTER METHODS
+	def name
+		@name
+	end
 
-  #breed setter method
-  def breed=(new_breed)
-    @breed = new_breed
-  end
+	def breed
+		@breed
+	end
 end
