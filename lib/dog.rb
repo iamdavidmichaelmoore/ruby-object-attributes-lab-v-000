0ad58340925b7174477ce7a21cff2e351d40df12
David Moore <hey@iamdavidmoore.com>
2018-04-13 15:50:54 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+  def initialize(name)
+    @name = name
+  end
+
+  #NAME GETTER AND SETTER METHODS
+  #name getter method
+  def name
+    @name
+  end
+
+  #name setter method
+  def name=(new_name)
+    @name = new_name
+  end
+
+  #BREED GETTER AND SETTER methods
+  #breed getter method
+  def breed
+    @breed
+  end
+
+  #breed setter method
+  def breed=(new_breed)
+    @breed = new_breed)
+  
+end
